f086e83e33523ae82cf68510f094cf5dd10660f1
Sirajuddin Asjad <sirasjad@gmail.com>
2017-09-07 22:25:21 +0200

    Completed task 5

--- LED_with_Potmeter/LED_with_Potmeter.ino
+++ LED_with_Potmeter/LED_with_Potmeter.ino
@@ -2,24 +2,33 @@
 
 // Variables
 int led = 7;
+int buzzer = 2;
 int petro = A1;
 int petroValue = 0;
 
 void setup(){
   pinMode(led, OUTPUT);
   pinMode(petro, INPUT);
+  pinMode(buzzer, OUTPUT);
+  //Serial.begin(9600); // For debugging
 }
 
 void loop(){
   petroValue = analogRead(petro);
+  //Serial.println(petroValue); // For debugging
 
-  Serial.print(petroValue);
-
-  if(petroValue <= 300){
+  if(petroValue > 0 && petroValue <= 300){
     digitalWrite(led, HIGH);
   }
   else{
     digitalWrite(led, LOW);
+  }
+
+  if(petroValue >= 1000){
+    tone(buzzer, 1000);
+  }
+  else{
+    noTone(buzzer);
   }
 }
 
