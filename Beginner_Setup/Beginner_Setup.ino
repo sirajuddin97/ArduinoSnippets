67a095e3f9343c1bed4461dc7e4f0385d1e4aed5
Sirajuddin Asjad <sirasjad@gmail.com>
2017-09-08 10:50:37 +0200

    WIP

--- Beginner_Setup/Beginner_Setup.ino
+++ Beginner_Setup/Beginner_Setup.ino
@@ -2,11 +2,23 @@ int heltall = 5;
 float desimaltall = 2.345;
 String tekst = "hei";
 char tegn = 's';
+long stortTall = 100000000000;
+boolean sannEllerUsann = true;
+int globalVariabel = 67;
 
 void setup() {
-  //
+  Serial.begin(9600);
 }
 
 void loop() {
-  //
+  Serial.println(tegn);
+  Serial.println((int)tegn);
+
+  if(true){
+    int nyVariabel = 4;
+  }
+
+  static int tall = 0;
+  Serial.println(tall);
+  tall++;
 }
