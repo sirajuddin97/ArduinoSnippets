c082e08d483e8487e2e12b84a12b6a9e781dccf5
Sirajuddin Asjad <sirasjad@gmail.com>
2017-09-22 10:46:42 +0200

    New task

--- Calculator/Calculator.ino
+++ Calculator/Calculator.ino
@@ -6,10 +6,12 @@ void loop() {
   int tall1, tall2, svar;
   Serial.println("Skriv inn to heltall");
 
-  while(Serial.available() == 0){
-    tall1 = Serial.parseInt();
-    tall2 = Serial.parseInt();
-    Serial.println(tall1);
-    Serial.println(tall2);
-  }
+  while(Serial.available() == 0){} // Pause
+  tall1 = Serial.parseInt();
+  
+  while(Serial.available() == 0){} // Pause
+  tall2 = Serial.parseInt();
+  
+  Serial.println(tall1);
+  Serial.println(tall2);
 }
