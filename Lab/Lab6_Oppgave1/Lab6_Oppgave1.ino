8cb602666198a26739186335778656915060d162
Sirajuddin Asjad <sirasjad@gmail.com>
2017-10-12 13:23:24 +0200

    Update

---
+++ Lab/Lab6_Oppgave1/Lab6_Oppgave1.ino
@@ -0,0 +1,40 @@
+// DFDP1100-1 17H Digitale systemer og programmering | Lab 6, Adresser og pekere
+// Oppgave 1:
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  int tall1 = -2;
+  int tall2 = -7;
+  int tall3 = -4;
+
+  tall1 = abs1(tall1);
+  abs2(tall2);
+  abs3(&tall3);
+
+  Serial.print(tall1);
+  Serial.print(tall2);
+  Serial.println(tall3);
+
+  delay(1000);
+}
+
+int abs1(int input){
+  // Vi velger å bruke int istedenfor void fordi vi ønsker å returnere noe tilbake til funksjonen
+  int tall = abs(input);
+  return tall;
+}
+
+void abs2(int &input){
+  // Her kan vi bruke void fordi vi ikke har noe å returnere tilbake til funksjonen
+  int tall = abs(input);
+  input = tall;
+}
+
+void abs3(int* input){
+  // Her kan vi bruke void fordi vi ikke har noe å returnere tilbake til funksjonen
+  int tall = abs(*input);
+  *input = tall;
+}
