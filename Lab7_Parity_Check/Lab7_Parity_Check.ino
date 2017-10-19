6935c9fb8416b63fbc9b551fc0e0ebf45ed8a40e
Sirajuddin Asjad <sirasjad@gmail.com>
2017-10-19 15:09:23 +0200

    Lab7

---
+++ Lab7_Parity_Check/Lab7_Parity_Check.ino
@@ -0,0 +1,17 @@
+// DFDP1100-1 17H Digitale systemer og programmering | Lab 7, Oppgave 2:
+
+void setup() {
+  int yellowPin = 8;
+  int redPin = 7;
+  int bluePin = 6;
+
+  pinMode(yellowPin, OUTPUT);
+  pinMode(redPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(yellowPin, HIGH);
+  digitalWrite(redPin, HIGH);
+  digitalWrite(bluePin, HIGH);
+}
