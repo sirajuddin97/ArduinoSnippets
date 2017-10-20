86eb8350c4b81aeb5fba4ec6cb49f97fb782689d
Sirajuddin Asjad <sirasjad@gmail.com>
2017-10-20 11:14:53 +0200

    Update

--- Beginner_Setup/Structures.ino
+++ Beginner_Setup/Structures.ino
@@ -6,7 +6,6 @@ struct data{
   int buttonValue;
   unsigned long timeStamp;
 };
-
 data measurements[10];
 
 void setup(){
@@ -18,14 +17,25 @@ void setup(){
 void loop(){
   measure();
   printOut();
-  delay(1000);
+  delay(10000);
 }
 
 void measure(){
-  //
+  for(int i = 0; i < 10; i++){
+    measurements[i].buttonValue = digitalRead(buttonPin);
+    measurements[i].potValue = analogRead(potPin);
+    measurements[i].timeStamp = millis();
+    delay(500);
+  }
 }
 
 void printOut(){
-  //
+  for(int i = 0; i < 10; i++){
+    Serial.print(measurements[i].buttonValue);
+    Serial.print("\t");
+    Serial.print(measurements[i].potValue);
+    Serial.print("\t");
+    Serial.println(measurements[i].timeStamp);
+  }
 }
 
